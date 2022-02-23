c3b9a3da7ad77d3603a0de5f5fbc9807b9c7fb5d
maynardrodgers <maynardrodgers@gmail.com>
2022-02-24 00:39:43 +0700

    commit message

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StoneFinance-pg/precache-manifest.fae10235c8b2ac3c33362e507796bba7.js"
+  "/precache-manifest.aafbb157166e95d17ec15e67a8d81975.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StoneFinance-pg/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
